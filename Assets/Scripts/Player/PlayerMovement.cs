ae9565338db462341e56831dce95c7a4cc3c7716
Archimedes Trajano <developer@trajano.net>
2017-06-17 02:21:54 -0400

    Finished 02-Player Character
    Needed to remove cameraRayLength parameter in order to get the turning
    to work.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,58 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidBody;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    private void Turning()
+    {
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        //if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        if (Physics.Raycast(camRay, out floorHit, floorMask))
+        {
+            var playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+            var newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+
+    private void Animating(float h, float v)
+    {
+        var walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
 }
