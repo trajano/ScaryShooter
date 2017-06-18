9b212cc23d2b3a0329effdb54008002ea58372db
Archimedes Trajano <developer@trajano.net>
2017-06-18 02:44:27 -0400

    Added a picture of Phoenix

---
+++ Assets/Scripts/Player/Picture.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picture : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public Texture2D veryHealthyPicture;
+    public Texture2D healthyPicture;
+    public Texture2D notHealthyPicture;
+    public Texture2D dyingPicture;
+    private Renderer pictureRenderer;
+    private void Awake()
+    {
+        pictureRenderer = GetComponent<Renderer>();
+    }
+    private void FixedUpdate()
+    {
+        if (playerHealth.currentHealth > 90)
+        {
+            pictureRenderer.material.mainTexture = veryHealthyPicture;
+        }
+        else if (playerHealth.currentHealth > 50)
+        {
+            pictureRenderer.material.mainTexture = healthyPicture;
+        }
+        else if (playerHealth.currentHealth > 25)
+        {
+            pictureRenderer.material.mainTexture = notHealthyPicture;
+        }
+        else if (playerHealth.currentHealth > 1)
+        {
+            pictureRenderer.material.mainTexture = dyingPicture;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+
+        transform.up = Camera.main.transform.position - transform.position;
+        transform.forward = -Camera.main.transform.up;
+    }
+}
